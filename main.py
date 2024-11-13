afb920824cecce16b7a51b7ae52d1219642257f3
Seregun <seregunmgp@gmail.com>
2024-11-13 09:51:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def anagrams(string1, string2): # Функция
-    string1 = string1.replace(" ","").lower() # Убирает пробелы первой строки
-    string2 = string2.replace(" ","").lower() # Убирает пробелы второй строки
-    return sorted(string1) == sorted(string2) # Возвращение полученного результата
 string1 = input("Введите первую строку: ") # Ввод первой строки
 string2 = input("Введите вторую строку: ") # Ввод второй строки
-if anagrams(string1, string2): # Условие (задание функции)
+string1 = string1.replace(" ","").lower() # Убирает пробелы первой строки
+string2 = string2.replace(" ","").lower() # Убирает пробелы второй строки
+if (sorted(string1) == sorted(string2)): # Условие (задание функции)
     print("Строки являются анаграммами") # Вывод: Строки являются анаграммами
 else: # Если строки не анаграммы
     print("Строки не являются анаграммами") # Вывод: Строки не являются анаграммами
